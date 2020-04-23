faf2de627aab82730a300493280c1c470e01332c
slava <slava.rubej@gmail.com>
2020-04-23 17:05:41 +0300

    migrate to postgres

--- budgetta/settings.py
+++ budgetta/settings.py
@@ -77,10 +77,18 @@ WSGI_APPLICATION = 'budgetta.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'budgetta_db',
+         'USER': 'budgetta_user',
+         'PASSWORD': 'budgetta_password',
+         'HOST': 'localhost',
+         'PORT': '54320',
+     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
 }
 
 
