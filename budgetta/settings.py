49a069c1fcd537fe603c2dbce0e73e6fd46bfc11
slava <slava.rubej@gmail.com>
2020-05-03 05:46:23 +0300

    fix income progress bar

--- budgetta/settings.py
+++ budgetta/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -23,18 +22,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g3-mo*$7%vi=_!l*^gqv-z+*wnn2b+u$@dg1$6-c40^3b%*&97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG_MODE',True)
+DEBUG = os.getenv('DEBUG_MODE', True)
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED', 'localhost')]
-
 
 # Application definition
 
 INSTALLED_APPS = [
-    #MY_APP
+    # MY_APP
+
     'budgetta_app',
     'users',
     'bootstrap4',
+    'bootstrap_modal_forms',
     'django_template_maths',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -74,25 +74,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'budgetta.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'budgetta_db',
-         'USER': 'budgetta_user',
-         'PASSWORD': 'budgetta_password',
-         'HOST': os.getenv('DB_HOST','localhost'),
-         'PORT': os.getenv('DB_PORT',"54320"),
-     }
-    #'default': {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'budgetta_db',
+        'USER': 'budgetta_user',
+        'PASSWORD': 'budgetta_password',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', "54320"),
+    }
+    # 'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   # }
+    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -112,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -126,15 +123,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
 
 # My settings
 LOGIN_URL = 'users:login'
